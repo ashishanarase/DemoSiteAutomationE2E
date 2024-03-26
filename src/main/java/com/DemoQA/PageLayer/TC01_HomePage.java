69ecacd60dce62dc369f904fc537f0b3f1dc8bff
Ashish <anaraseashish@gmail.com>
2024-03-26 23:23:33 +0530

    NewJSS

--- src/main/java/com/DemoQA/PageLayer/TC01_HomePage.java
+++ src/main/java/com/DemoQA/PageLayer/TC01_HomePage.java
@@ -1,7 +1,26 @@
 package com.DemoQA.PageLayer;
 
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Point;
+
 import com.DemoQA.TestBase.TestBase;
 
 public class TC01_HomePage extends TestBase {
+
+    public void actionOnBrowser() throws InterruptedException {
+		
+		Dimension size = driver.manage().window().getSize();
+		System.out.println(size);
+		Dimension d = new Dimension(980, 580);
+		driver.manage().window().setSize(d);
+		
+		Point positon = driver.manage().window().getPosition();
+		System.out.println(positon);
+		Point p = new Point (0,0);
+		driver.manage().window().setPosition(p);
+		
+		Thread.sleep(3000);
+		
+	}
 
 }
