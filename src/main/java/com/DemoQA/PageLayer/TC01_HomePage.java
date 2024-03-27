c69ca361dff37234a5285cabe0f65c32a9fbaabe
Ashish Anarase <anaraseashish@gmail.com>
2024-03-27 12:30:21 +0530

    New

--- src/main/java/com/DemoQA/PageLayer/TC01_HomePage.java
+++ src/main/java/com/DemoQA/PageLayer/TC01_HomePage.java
@@ -2,25 +2,34 @@ package com.DemoQA.PageLayer;
 
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import com.DemoQA.TestBase.TestBase;
 
 public class TC01_HomePage extends TestBase {
 
-    public void actionOnBrowser() throws InterruptedException {
-		
+	//------------ Xpath Repository -------------
+
+	@FindBy (xpath = " ")
+	private WebElement txtbox_userName_homePage;
+
+	//------------ Action Methods -------------	
+
+	public void actionOnBrowser() throws InterruptedException {
+
 		Dimension size = driver.manage().window().getSize();
 		System.out.println(size);
 		Dimension d = new Dimension(980, 580);
 		driver.manage().window().setSize(d);
-		
+
 		Point positon = driver.manage().window().getPosition();
 		System.out.println(positon);
 		Point p = new Point (0,0);
 		driver.manage().window().setPosition(p);
-		
+
 		Thread.sleep(3000);
-		
+
 	}
 
 }
